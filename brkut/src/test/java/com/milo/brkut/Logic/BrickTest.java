9c1bb98b469a36cc2276cacb6f4d9b8ea9126a89
Mikko Loponen <mikko.loponen@iki.fi>
2014-11-05 13:30:41 +0200

    1st JUnit test, test dir added

---
+++ brkut/src/test/java/com/milo/brkut/Logic/BrickTest.java
@@ -0,0 +1,31 @@
+package com.milo.brkut.Logic;
+
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author milo
+ */
+public class BrickTest {
+    
+    public BrickTest() {
+    }
+    
+    /**
+     * Test of damage method, of class Brick.
+     */
+    @Test
+    public void testHPandDamage() {
+        System.out.println("damage");
+        Brick instance = new Brick(0,0,10,10);
+        assertEquals(2, instance.hp());
+        instance.damage(3);
+        // TODO review the generated test code and remove the default call to fail.
+        assertEquals(-1, instance.hp());
+    }
+}
