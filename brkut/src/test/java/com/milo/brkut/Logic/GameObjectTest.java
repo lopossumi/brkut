a1a25379204add5a22dfbb8df0c7711f71021169
Mikko Loponen <mikko.loponen@iki.fi>
2014-11-05 14:55:15 +0200

    collision detection

---
+++ brkut/src/test/java/com/milo/brkut/Logic/GameObjectTest.java
@@ -0,0 +1,43 @@
+package com.milo.brkut.Logic;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author milo
+ */
+public class GameObjectTest {
+    
+    public GameObjectTest() {
+    }
+    
+    /**
+     * Testing collision between the ball and a brick.
+     */
+    @Test
+    public void testCollisionBallBrick() {
+        System.out.println("Brick and ball collision");
+        GameObject ball = new Ball(0,0,10,10);
+        GameObject brick = new Brick(100,100,100,10);
+        //no collision
+        assertEquals(0, ball.collision(brick));
+        
+        //vertical collision from bottom
+        ball.move(60,91);
+        assertEquals(-1, ball.collision(brick));
+        }
+
+    /**
+     * Test of move method, of class GameObject.
+     */
+    @Test
+    public void testMove() {
+        System.out.println("Move");
+        GameObject instance = new Ball(0,0,10,10);
+        instance.move(10, 10);
+        assertEquals(10.0, instance.getX(),0.0);
+        assertEquals(10.0, instance.getY(),0.0);
+        }
+
+}
