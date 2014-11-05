eebc78ec4851b2769a670dd290ef6c4a21cf5cbf
Mikko Loponen <mikko.loponen@iki.fi>
2014-11-05 22:21:52 +0200

    i thought i did these already...?

--- brkut/src/main/java/com/milo/brkut/Logic/Arena.java
+++ brkut/src/main/java/com/milo/brkut/Logic/Arena.java
@@ -7,29 +7,40 @@ import java.util.ArrayList;
  */
 public class Arena {
 
-	private Player playerOne;
-	private Ball ball;
-	private ArrayList<Brick> bricks;
-	private GameStatus status;
+    private Player playerOne;
+    private Ball ball;
+    private ArrayList<Brick> bricks;
+    private GameStatus status;
 
-	public Arena() {
-		this.status = GameStatus.RUNNING;
-	}
+    public Arena() {
+        this.status = GameStatus.RUNNING;
+    }
 
-	/**
-	 * Do operations for this step. - move stuff - check collisions and
-	 * deaths
-	 */
-	public void step() {
-		this.updateStatus();
-	}
+    /**
+     * Do operations for this step. - move stuff - check collisions and deaths
+     */
+    public void step() {
+        this.updateStatus();
+    }
 
-	public void updateStatus() {
-		if (bricks.isEmpty()) {
-			this.status = GameStatus.WON;
-		}
-		if (playerOne.getLives() == 0) {
-			this.status = GameStatus.GAMEOVER;
-		}
-	}
+    public void updateStatus() {
+        if (bricks.isEmpty()) {
+            this.status = GameStatus.WON;
+        }
+        if (playerOne.getLives() == 0) {
+            this.status = GameStatus.GAMEOVER;
+        }
+    }
+
+    public ArrayList<Brick> getBricks() {
+        return this.bricks;
+    }
+
+    public Player getPlayerOne() {
+        return this.playerOne;
+    }
+
+    public Ball getBall() {
+        return this.ball;
+    }
 }
