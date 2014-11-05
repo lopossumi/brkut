c388c008042ee083e9d30fd9dd65c9af9cf44583
Mikko Loponen <mikko.loponen@iki.fi>
2014-11-05 14:55:00 +0200

    collision detection

--- brkut/src/main/java/com/milo/brkut/Logic/GameObject.java
+++ brkut/src/main/java/com/milo/brkut/Logic/GameObject.java
@@ -5,49 +5,115 @@ package com.milo.brkut.Logic;
  */
 public abstract class GameObject {
 
-        // Bounding box coordinates
-	private double left;
-	private double right;
-	private double top;
-	private double bottom;
+    // Bounding box coordinates
+    private double x;
+    private double y;
+    private double width;
+    private double height;
 
-	// Speed components
-	private double vx;
-	private double vy;
+    // Speed components
+    private double vx;
+    private double vy;
 
-	public GameObject(double x, double y, double width, double height) {
-		this.left = x - width / 2;
-		this.right = x + width / 2;
-		this.top = y - height / 2;
-		this.bottom = y + height / 2;
-		this.vx = 0;
-		this.vy = 0;
-	}
+    public GameObject(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.vx = 0;
+        this.vy = 0;
+    }
 
-	public void move(double x, double y) {
-		this.left += x;
-		this.right += x;
-		this.top += y;
-		this.bottom += y;
-	}
+    public double getX() {
+        return x;
+    }
 
-	public void move() {
-		this.left += vx;
-		this.right += vx;
-		this.top += vy;
-		this.bottom += vy;
-	}
+    public double getY() {
+        return y;
+    }
 
-	public void accelerateX(double amount) {
-		this.vx += amount;
-	}
+    public double getWidth() {
+        return width;
+    }
 
-	public void accelerateY(double amount) {
-		this.vy += amount;
-	}
+    public double getHeight() {
+        return height;
+    }
 
-	public void accelerateXY(double amountX, double amountY) {
-		this.accelerateX(amountX);
-		this.accelerateY(amountY);
-	}
+    public double getVx() {
+        return vx;
+    }
+
+    public double getVy() {
+        return vy;
+    }
+
+    /**
+     * Moves the object (dx,dy) units.
+     *
+     * @param dx X coordinate difference
+     * @param dy Y coordinate difference
+     */
+    public void move(double dx, double dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    /**
+     * Moves the object according to its speed.
+     */
+    public void move() {
+        this.x += vx;
+        this.y += vy;
+    }
+
+    /**
+     * Increases the object's speed on the X axis.
+     *
+     * @param amount
+     */
+    public void accelerateX(double amount) {
+        this.vx += amount;
+    }
+
+    /**
+     * Increases the object's speed on the Y axis.
+     *
+     * @param amount
+     */
+    public void accelerateY(double amount) {
+        this.vy += amount;
+    }
+
+    /**
+     * Increases the object's speed on both axes.
+     *
+     * @param amountX
+     * @param amountY
+     */
+    public void accelerateXY(double amountX, double amountY) {
+        this.accelerateX(amountX);
+        this.accelerateY(amountY);
+    }
+
+    /**
+     * Checks collision between two objects (this and the other).
+     *
+     * @param other the second object
+     * @return -1 for vertical collision, 1 for horizontal, 0 for no collision.
+     * For corner cases the default is vertical collision.
+     */
+    public int collision(GameObject other) {
+        double xOverlapAmount = -Math.abs(this.getX() - other.getX()) + (this.getWidth() + other.getWidth()) / 2;
+        double yOverlapAmount = -Math.abs(this.getY() - other.getY()) + (this.getHeight() + other.getHeight()) / 2;
+
+        // No collision (overlap <= 0 on either axis)
+        if (xOverlapAmount <= 0 || yOverlapAmount <= 0) {
+            return 0;
+        } else if (xOverlapAmount > yOverlapAmount) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
 }
