58cefb745fa6548e1e7ff30ea8ac5f6d9235cd48
Mikko Loponen <mikko.loponen@iki.fi>
2014-11-10 13:35:06 +0200

    keylistener stub

--- brkut/src/main/java/com/milo/brkut/Main/UserInput.java
+++ brkut/src/main/java/com/milo/brkut/Main/UserInput.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.milo.brkut.Main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  *
  * @author milo
  */
-public class UserInput {
+public class UserInput implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
